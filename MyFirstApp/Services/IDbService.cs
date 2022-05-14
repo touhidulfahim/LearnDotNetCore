26effebbade8fa52a265d61be521597656fcc480
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2022-05-14 12:38:32 +0600

    all basic configuratios and nugat

---
+++ MyFirstApp/Services/IDbService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Services
+{
+    public interface IDbService
+    {
+    }
+}
